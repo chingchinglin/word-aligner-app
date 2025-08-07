b3f4602aca7908faf0f92e83bdbcdd2ed6b3c7a3
chingchinglin <sindylin319@gmail.com>
2025-08-07 16:06:40 +0800

    Add files via upload

--- post-install.sh
+++ post-install.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+echo "✅ post-install.sh started"
 python -m spacy download en_core_web_sm
+echo "✅ finished downloading model"
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
+
 import streamlit as st
 from aligner import get_example_alignment
 
 st.title("🔍 Word Aligner (Safe Version)")
 start, end = get_example_alignment()
 st.write(f"測試對齊結果：start={start}, end={end}")
-
-
