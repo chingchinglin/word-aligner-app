0a34b5f9c335ac843801dbcb8498711ed0b9306d
chingchinglin <sindylin319@gmail.com>
2025-08-07 14:25:33 +0800

    Update aligner.py

--- aligner.py
+++ aligner.py
@@ -1,12 +1,11 @@
 import spacy
 import pandas as pd
-import subprocess
 
-# 嘗試載入 spaCy 模型，如果沒安裝就自動安裝
 try:
     nlp = spacy.load("en_core_web_sm")
 except OSError:
-    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"], check=True)
+    from spacy.cli import download
+    download("en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
 
 def find_match_indices(word_or_phrase, sentence):
